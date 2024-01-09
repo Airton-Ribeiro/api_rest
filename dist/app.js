f9f3cccbbd56c8cca44a9c092bce69633f6c7d2e
Airton Ribeiro <airtondeodato2007@hotmail.com>
2024-01-09 16:58:27 -0300

    adicionando build

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _path = require('path');
 _dotenv2.default.config();
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 
 require('./database');
 
@@ -11,7 +12,8 @@ var _token = require('./routes/token'); var _token2 = _interopRequireDefault(_to
 var _aluno = require('./routes/aluno'); var _aluno2 = _interopRequireDefault(_aluno);
 var _picture = require('./routes/picture'); var _picture2 = _interopRequireDefault(_picture);
 
-class App { // criando uma classe para exportar o necessario para o servidor
+class App {
+  // criando uma classe para exportar o necessario para o servidor
   constructor() {
     // construtor onde serão executados todos os metodos quando a classe for instanciada
     this.app = _express2.default.call(void 0, ); // instanciando o express no .app
@@ -19,21 +21,26 @@ class App { // criando uma classe para exportar o necessario para o servidor
     this.routes(); // metodo routes
   }
 
-  middlewares() { // middlewar para executar pelo construtor
+  middlewares() {
+    this.app.use(_cors2.default.call(void 0, ));
+    // middlewar para executar pelo construtor
     this.app.use(_express2.default.urlencoded({ extended: true }));
     /* Este middleware acima é responsável por analisar os dados
      enviados pelo cliente através de formulários HTML */
     this.app.use(_express2.default.json());
     // Este middleware acima é responsável por analisar o corpo de solicitações com formato JSON.
-    this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
+    this.app.use(
+      "/images/",
+      _express2.default.static(_path.resolve.call(void 0, __dirname, "..", "uploads", "images"))
+    );
   }
 
   routes() {
-    this.app.use('/', _home2.default); // rota para a pagina home
-    this.app.use('/users/', _user2.default);
-    this.app.use('/tokens/', _token2.default);
-    this.app.use('/alunos/', _aluno2.default);
-    this.app.use('/picture/', _picture2.default);
+    this.app.use("/", _home2.default); // rota para a pagina home
+    this.app.use("/users/", _user2.default);
+    this.app.use("/tokens/", _token2.default);
+    this.app.use("/alunos/", _aluno2.default);
+    this.app.use("/picture/", _picture2.default);
   }
 }
 
